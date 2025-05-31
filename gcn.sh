6de24b82a768120d29054a14e22eff8ac590bbd9
Xueqi Cheng <xueqi.cheng@vanderbilt.edu>
2025-05-31 17:15:19 -0500

    Update gcn.sh

--- gcn.sh
+++ gcn.sh
@@ -6,12 +6,14 @@ output_file="./final_result/reweight.txt"
 >> $output_file
 
 
-# Setting 1 (no threshold needed)
+## Definition I
 # python main.py --dataset='ba' --setting=1 --model=GCN --n_emb=32 --n_hidden=1024 --n_out=16 >> $output_file
 
+## Definition II and III (global threshold)
 # python main.py --dataset='ba' --setting=2 --model=GCN --thre=5 --n_emb=32 --n_hidden=1024 --n_out=16 >> $output_file
 # python main.py --dataset='ba' --setting=3 --model=GCN --thre=5 --n_emb=32 --n_hidden=1024 --n_out=16 >> $output_file
 
+## Definition IV to VII (local threshold)
 # for setting in {4..7}
 # do
 #     python main.py --dataset='ba' --setting=$setting --model=GCN --thre=0.2 --n_emb=32 --n_hidden=1024 --n_out=16 >> $output_file
@@ -21,13 +23,9 @@ output_file="./final_result/reweight.txt"
 # # # Constants
 tw="2e7"
 
-# # Setting 1 (specific case without threshold)
 # python main.py --dataset='msg' --setting=1 --model=GCN --n_emb=32 --n_hidden=512 --n_out=32 >> $output_file
-
-# # Settings 2 (specific thresholds)
 # python main.py --dataset='msg' --setting=2 --model=GCN --thre=5 --tw=$tw --n_emb=32 --n_hidden=512 --n_out=32 >> $output_file
 # python main.py --dataset='msg' --setting=3 --model=GCN --thre=5 --tw=$tw --n_emb=32 --n_hidden=512 --n_out=32 >> $output_file
-
 # for setting in {4..7}
 # do
 #     python main.py --dataset='msg' --setting=$setting --model=GCN --thre=0.2 --tw=$tw --n_emb=32 --n_hidden=512 --n_out=32 >> $output_file
