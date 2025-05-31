f7459d539ae2a385260b6dc4623404cb11eb30d9
Xueqi Cheng <xueqi.cheng@vanderbilt.edu>
2025-05-31 17:18:25 -0500

    Update tran.sh

--- tran.sh
+++ tran.sh
@@ -5,13 +5,13 @@ output_file="final_result/reweight.txt"
 # Clear the previous content of the output file if it exists
 >> $output_file
 
-# Setting 1 (no threshold needed)
+
+## Definition I 
 # python main.py --dataset='ba' --setting=1 --model=Tran --n_emb=1024 --n_hidden=32 --n_out=16 >> $output_file
-
-
+## Definition II and III (global threshold)
 # python main.py --dataset='ba' --setting=2 --model=Tran --thre=5 --n_emb=1024 --n_hidden=32 --n_out=16 >> $output_file
 # python main.py --dataset='ba' --setting=3 --model=Tran --thre=5 --n_emb=1024 --n_hidden=32 --n_out=16 >> $output_file
-
+## Definition IV to VII (loacl threshold)
 # for setting in {4..7}
 # do
 #     python main.py --dataset='ba' --setting=$setting --model=Tran --thre=0.2 --n_emb=1024 --n_hidden=32 --n_out=16 >> $output_file
@@ -22,17 +22,12 @@ output_file="final_result/reweight.txt"
 # # # Constants
 tw="2e7"
 
-# # Setting 1 (specific case without threshold)
 # python main.py --dataset='msg' --setting=1 --model=Tran --tw=$tw --n_emb=64 --n_hidden=256 --n_out=64 >> $output_file
-
-
 # python main.py --dataset='msg' --setting=2 --model=Tran --thre=5 --tw=$tw  --n_emb=64 --n_hidden=256 --n_out=64 >> $output_file
 # python main.py --dataset='msg' --setting=3 --model=Tran --thre=5 --tw=$tw  --n_emb=64 --n_hidden=256 --n_out=64 >> $output_file
-
 # for setting in {4..7}
 # do
 #     python main.py --dataset='msg' --setting=$setting --model=Tran --thre=0.2 --tw=$tw  --n_emb=64 --n_hidden=256 --n_out=64 >> $output_file
-
 # done
 
 
