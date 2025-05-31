6ae764ccd67622ba149a5ad6b2db32630ef63a63
Xueqi Cheng <xueqi.cheng@vanderbilt.edu>
2025-05-31 17:17:24 -0500

    Update rf.sh

--- rf.sh
+++ rf.sh
@@ -4,12 +4,12 @@ output_file="final_result/rf.txt"
 # Clear the previous content of the output file if it exists
 >> $output_file
 
-# Setting 1 (no threshold needed)
+## Definition I
 # python main.py --dataset='ba' --setting=1 --model=rf >> $output_file
-
+## Definition II and III (global thresold)
 # python main.py --dataset='ba' --setting=2 --model=rf --thre=5 >> $output_file
 # python main.py --dataset='ba' --setting=3 --model=rf --thre=5 >> $output_file
-
+## Definition IV to VII (local threshold)
 # for setting in {4..7}
 # do
 #     python main.py --dataset='ba' --setting=$setting --model=rf --thre=0.2 >> $output_file
@@ -19,16 +19,12 @@ output_file="final_result/rf.txt"
 # # # Constants
 # tw="2e7"
 
-# # Setting 1 (specific case without threshold)
 # python main.py --dataset='msg' --setting=1 --tw=$tw --model=rf >> $output_file
 # python main.py --dataset='msg' --setting=2 --thre=5 --tw=$tw --model=rf >> $output_file
 # python main.py --dataset='msg' --setting=3 --thre=5 --tw=$tw --model=rf >> $output_file
-
 # for setting in {4..7}
 # do
-
 #     python main.py --dataset='msg' --setting=$setting --thre=0.2 --tw=$tw --model=rf >> $output_file
-
 # done
 
 python main.py --dataset='tw' --setting=1 --model=rf --n_emb=64 --n_hidden=1024 --n_out=32 >> $output_file
